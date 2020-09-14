2c63022e5b33ccd11ed4ba01a89e3a12aa4d19a0
Dorota Puto <dootras@gmail.com>
2020-09-14 21:08:18 +0200

    Rendering favourite books

--- src/components/BookDetailsPage.js
+++ src/components/BookDetailsPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { addBookToFavourites, removeBookFromFavourites } from '../actions/favourites';
 
 class BookDetailsPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            ...props.book
-        }
+
+        this.state = props.book ? { ...props.book } : { ...props.isFavourite }
     }
 
     onClick = () => window.open(this.props.book.previewLink, "_blank");
@@ -17,19 +15,19 @@ class BookDetailsPage extends React.Component {
         if (!this.props.isFavourite) {
             this.props.dispatch(addBookToFavourites({ ...this.state }));
         } else {
-            this.props.dispatch(removeBookFromFavourites({id: this.state.id}))
+            this.props.dispatch(removeBookFromFavourites({ id: this.state.id }))
         }
     }
 
     render() {
         return (
             <div>
-                <img src={this.props.book.thumbnail} />
+                <img src={this.state.thumbnail} />
                 <h3>Title: {this.state.title}</h3>
                 {this.state.subtitle && <p>Subtitle: {this.state.subtitle}</p>}
                 {this.state.authors && <p>Authors: {this.state.authors}</p>}
                 {this.state.publisher && <p>Publisher: {this.state.publisher}</p>}
-                <div dangerouslySetInnerHTML={{ __html: this.props.book.description }} />
+                <div dangerouslySetInnerHTML={{ __html: this.state.description }} />
                 <button onClick={this.onClick}>PREVIEW</button>
                 <button
                     onClick={this.onClickFavourite}
@@ -37,14 +35,14 @@ class BookDetailsPage extends React.Component {
                     <ion-icon name="heart"></ion-icon>
                 </button>
             </div>
-        )
-    }
-};
-const mapStateToProps = (state, props) => {
-    return {
-        book: state.books.find((book) => book.id === props.match.params.id),
-        favourites: state.favourites,
-        isFavourite: state.favourites.find((book) => book.id === props.match.params.id)
+        );
     }
 }
-export default connect(mapStateToProps)(BookDetailsPage);
+    const mapStateToProps = (state, props) => {
+        return {
+            book: state.books.find((book) => book.id === props.match.params.id),
+            favourites: state.favourites,
+            isFavourite: state.favourites.find((book) => book.id === props.match.params.id)
+        }
+    }
+    export default connect(mapStateToProps)(BookDetailsPage);
--- src/components/FavouritesPage.js
+++ src/components/FavouritesPage.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import BookItem from './BookItem';
 
 class FavouritesPage extends React.Component {
 
   render() {
     return (
       <div>
-        This is from my favourite page component
+        {this.props.books && this.props.books.map((book) => {
+          return <BookItem key={book.id} {...book} />
+        })}
       </div>
     )
   }
 };
 
-export default FavouritesPage;
+const mapStateToProps = (state) => {
+  return {
+      books: state.favourites
+  };
+};
+
+export default connect(mapStateToProps)(FavouritesPage);
